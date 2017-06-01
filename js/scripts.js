8e30d0ea252f542da6cb356711925f9913eb4add
Laura Hamilton and Mara Timberlake <laura+mara@>
2017-06-01 13:52:39 -0700

    add initial html and JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+
+var result = subtract(number1, number2);
+alert(result);
+
+
+/* From Mic's and my version
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 
@@ -33,4 +54,36 @@ var litersToCups = function(liters) {
 };
 
 var cups = litersToCups(liters);
-alert("Hey man, you got " + cups + " cups.");
+alert("Hey man, you got " + cups + " cups.");*/
+
+
+
+
+
+
+
+/*From partner's version
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    $("#output").text(result);
+  });
+});*/
